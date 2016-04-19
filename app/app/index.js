8a56fb21f8b142999e2e07ea820ccc21e1f85e99
Alejandro Diaz <alejandrodiazok@gmail.com>
2016-04-19 10:29:00 -0400

    all before delete machine

--- app/app/index.js
+++
@@ -1,4 +0,0 @@
-const angular = require('angular');
-const ngModule = angular.module('app',[]);
-
-require('./directives')(ngModule);
--- app/index.js
+++ app/index.js
@@ -1,4 +1,30 @@
 const angular = require('angular');
+const db = require('elasticsearch');
 const ngModule = angular.module('app',[]);
 
-require('./directives')(ngModule);
+var client = new db.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+});
+
+console.log(client);
+
+client.search({
+  method : 'get',
+  index: 'peluditos',
+  type: 'amigo',
+  body: {
+    "query" : {
+        "term" : { "familia" : "perro" }
+       }  
+    }
+}).then(function (resp) {
+    var hits = resp.hits.hits;
+	console.log(hits);
+}, function (err) {
+    console.trace(err.message);
+});
+
+
+//require('./directives')(ngModule);
+
